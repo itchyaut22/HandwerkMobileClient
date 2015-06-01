6e185d900109feaeaef275ec54f2c6dc186e62ac
Martin Pfoser <pfoser.martin@gmail.com>
2015-06-01 14:38:50 +0200

    filter and sort implementation

--- app/src/main/java/se/jku/at/handwerkmobileclient/fragments/ListFragment.java
+++ app/src/main/java/se/jku/at/handwerkmobileclient/fragments/ListFragment.java
@@ -5,12 +5,14 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ItemClick;
+import org.androidannotations.annotations.TextChange;
 import org.androidannotations.annotations.ViewById;
 
 import se.jku.at.handwerkmobileclient.R;
@@ -27,18 +29,43 @@ public class ListFragment extends Fragment implements AdapterView.OnItemSelected
     @ViewById(R.id.list_view)
     ListView stationList;
 
+    @ViewById(R.id.spinner1)
+    Spinner spinner;
+
+    @ViewById(R.id.swipe_container)
+    SwipeRefreshLayout swipeRefreshLayout;
+
     @Bean
     ServiceAdapter adapter;
 
     @AfterViews
     void init() {
         stationList.setAdapter(adapter);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshContent();
+            }
+        });
+        spinner.setOnItemSelectedListener(this);
+        spinner.setSelection(0);
+    }
+
+    @TextChange(R.id.inputSearch)
+    protected void onTextChangesOnInputSearch(CharSequence text, int before, int start, int count) {
+        adapter.getFilter().filter(text);
+    }
+
+    private void refreshContent() {
+        adapter.refresh();
+        swipeRefreshLayout.setRefreshing(false);
+        stationList.smoothScrollToPosition(0);
     }
 
     @ItemClick(R.id.list_view)
     protected void itemClicked(Service service) {
         // show detail view
-        Toast.makeText(getActivity(), service.getHeadline(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getActivity(), service.getHeadline(), Toast.LENGTH_SHORT).show();
 
         ServiceDetailActivity_.intent(getActivity()).id(service.getId()).start();
     }
@@ -46,11 +73,22 @@ public class ListFragment extends Fragment implements AdapterView.OnItemSelected
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+        setComparatorForAdapter(position);
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void setComparatorForAdapter(int id) {
+        switch(id) {
+            case 0:
+                adapter.setComparator(ServiceAdapter.serviceNameComparator);
+                break;
+            case 1:
+                adapter.setComparator(ServiceAdapter.servicePriceComparator);
+                break;
+        }
     }
 }
--- app/src/main/java/se/jku/at/handwerkmobileclient/model/ServiceAdapter.java
+++ app/src/main/java/se/jku/at/handwerkmobileclient/model/ServiceAdapter.java
@@ -4,36 +4,47 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import org.androidannotations.annotations.AfterInject;
 import org.androidannotations.annotations.EBean;
 import org.androidannotations.annotations.RootContext;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import se.jku.at.handwerkmobileclient.rest.impl.HandwerkResourceImpl;
 import se.jku.at.handwerkmobileclient.views.ListItemView;
-import se.jku.at.handwerkmobileclient.views.*;
+import se.jku.at.handwerkmobileclient.views.ListItemView_;
 
 /**
  * Created by Martin on 14.05.15.
  */
 @EBean
-public class ServiceAdapter extends BaseAdapter {
+public class ServiceAdapter extends BaseAdapter implements Filterable {
 
-
+    private Filter serviceFilter;
     private List<Service> services;
+    private List<Service> services_orig;
+    private Comparator<Service> comparator;
 
     @RootContext
     Context context;
 
     @AfterInject
     void initAdapter() {
+        comparator = serviceNameComparator;
         getData();
     }
 
     private void getData() {
+
         services = new HandwerkResourceImpl().getAllServices().getList();
+        services_orig = new ArrayList<>(services);
+        Collections.sort(services, comparator);
     }
 
     @Override
@@ -63,5 +74,78 @@ public class ServiceAdapter extends BaseAdapter {
 
         view.bind(getItem(position));
         return view;
+    }
+
+    public void refresh() {
+        getData();
+        notifyDataSetChanged();
+    }
+
+    public void setComparator(Comparator<Service> c) {
+        this.comparator = c;
+        refresh();
+    }
+
+    public static Comparator<Service> serviceNameComparator = new Comparator<Service>() {
+        @Override
+        public int compare(Service lhs, Service rhs) {
+            return lhs.getHeadline().compareTo(rhs.getHeadline());
+        }
+    };
+
+    public static Comparator<Service> servicePriceComparator = new Comparator<Service>() {
+        @Override
+        public int compare(Service lhs, Service rhs) {
+            return (int) (lhs.getPrice() - rhs.getPrice());
+        }
+    };
+
+    @Override
+    public Filter getFilter() {
+        if (serviceFilter == null) {
+            serviceFilter = new ServiceFilter();
+        }
+        return serviceFilter;
+    }
+
+    private class ServiceFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            if (constraint != null && constraint.length() > 0) {
+                List<Service> filterList = new ArrayList<>();
+                String constraintStr = constraint.toString().toLowerCase();
+                for (int i = 0; i < services.size(); i++) {
+                    String headline = services.get(i).getHeadline();
+                    String desc = services.get(i).getDetailInfo();
+
+                    if (headline != null && headline.toLowerCase().contains(constraintStr)) {
+                        filterList.add(services.get(i));
+                    }
+                    else if (desc != null && desc.toLowerCase().contains(constraintStr)) {
+                        filterList.add(services.get(i));
+                    }
+                }
+                results.count = filterList.size();
+                results.values = filterList;
+            } else {
+                synchronized (this) {
+                    results.count = services_orig.size();
+                    results.values = services_orig;
+                }
+            }
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            if (results.count == 0)
+                notifyDataSetInvalidated();
+            else {
+                services = (List<Service>) results.values;
+                notifyDataSetChanged();
+            }
+        }
     }
 }
